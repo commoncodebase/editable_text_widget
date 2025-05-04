5b2b4e6b66d31863b7b877de894d55680dffd0e0
hasanaliozkan-dev <hasanaliozkan@posta.mu.edu.tr>
2025-05-04 21:20:58 +0300

    it is implemented and test implementations are started

--- lib/editable_text_widget.dart
+++ lib/editable_text_widget.dart
@@ -1,5 +1,82 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class EditableTextWidget extends StatefulWidget {
+  // width and height of the editable text widget
+  final String text;
+  final double width;
+  final double height;
+  final double fontSize;
+  final Color color;
+  final Color backgroundColor;
+  final bool border;
+  final Color borderColor;
+  final double borderWidth;
+  final double borderRadius;
+  final double padding;
+  final double margin;
+  final double textWidth;
+  final double textHeight;
+  final double textPadding;
+  final double textMargin;
+  final Color textColor;
+  // if the border is true, then we create a border to our editable text widget
+
+  const EditableTextWidget({
+    super.key,
+    required this.text,
+    this.width = 0.1,
+    this.height = 0.03,
+    this.fontSize = 14.0,
+    this.color = Colors.black,
+    this.backgroundColor = Colors.white,
+    this.border = false,
+    this.borderColor = Colors.black,
+    this.borderWidth = 1.0,
+    this.borderRadius = 5.0,
+    this.padding = 0.0,
+    this.margin = 0.0,
+    this.textWidth = 0.1,
+    this.textHeight = 0.03,
+    this.textPadding = 0.0,
+    this.textMargin = 0.0,
+    this.textColor = Colors.black,
+  });
+
+  @override
+  EditableTextWidgetState createState() => EditableTextWidgetState();
+}
+
+class EditableTextWidgetState extends State<EditableTextWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: widget.width * MediaQuery.of(context).size.width,
+      height: widget.height * MediaQuery.of(context).size.height,
+      padding: EdgeInsets.all(widget.padding),
+      margin: EdgeInsets.all(widget.margin),
+      decoration: BoxDecoration(
+        color: widget.backgroundColor,
+        borderRadius: BorderRadius.circular(widget.borderRadius),
+        border:
+            widget.border
+                ? Border.all(
+                  color: widget.borderColor,
+                  width: widget.borderWidth,
+                )
+                : null,
+      ),
+      child: TextField(
+        controller: TextEditingController(text: widget.text),
+        style: TextStyle(fontSize: widget.fontSize, color: widget.color),
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.all(widget.textPadding),
+          hintText: 'Enter text',
+          hintStyle: TextStyle(
+            fontSize: widget.fontSize,
+            color: widget.textColor,
+          ),
+        ),
+      ),
+    );
+  }
 }
--- test/editable_text_widget_test.dart
+++ test/editable_text_widget_test.dart
@@ -1,12 +1,287 @@
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:editable_text_widget/editable_text_widget.dart';
+import 'package:flutter/material.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  testWidgets('EditableTextWidget has a default width and height', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Scaffold(body: EditableTextWidget(text: 'Hello')),
+      ),
+    );
+
+    final editableTextFinder = find.byType(EditableTextWidget);
+    expect(editableTextFinder, findsOneWidget);
+
+    final editableTextWidget = tester.widget<EditableTextWidget>(
+      editableTextFinder,
+    );
+    expect(editableTextWidget.width, 0.1);
+    expect(editableTextWidget.height, 0.03);
+  });
+
+  testWidgets('EditableTextWidget has a default font size', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Scaffold(body: EditableTextWidget(text: 'Hello')),
+      ),
+    );
+
+    final editableTextFinder = find.byType(EditableTextWidget);
+    expect(editableTextFinder, findsOneWidget);
+
+    final editableTextWidget = tester.widget<EditableTextWidget>(
+      editableTextFinder,
+    );
+    expect(editableTextWidget.fontSize, 14.0);
+  });
+
+  testWidgets('EditableTextWidget has a default color', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Scaffold(body: EditableTextWidget(text: 'Hello')),
+      ),
+    );
+
+    final editableTextFinder = find.byType(EditableTextWidget);
+    expect(editableTextFinder, findsOneWidget);
+
+    final editableTextWidget = tester.widget<EditableTextWidget>(
+      editableTextFinder,
+    );
+    expect(editableTextWidget.color, Colors.black);
+  });
+
+  testWidgets('EditableTextWidget has a default background color', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Scaffold(body: EditableTextWidget(text: 'Hello')),
+      ),
+    );
+
+    final editableTextFinder = find.byType(EditableTextWidget);
+    expect(editableTextFinder, findsOneWidget);
+
+    final editableTextWidget = tester.widget<EditableTextWidget>(
+      editableTextFinder,
+    );
+    expect(editableTextWidget.backgroundColor, Colors.white);
+  });
+
+  testWidgets('EditableTextWidget has a default border', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Scaffold(body: EditableTextWidget(text: 'Hello')),
+      ),
+    );
+
+    final editableTextFinder = find.byType(EditableTextWidget);
+    expect(editableTextFinder, findsOneWidget);
+
+    final editableTextWidget = tester.widget<EditableTextWidget>(
+      editableTextFinder,
+    );
+    expect(editableTextWidget.border, false);
+  });
+
+  testWidgets('EditableTextWidget has a default border color', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Scaffold(body: EditableTextWidget(text: 'Hello')),
+      ),
+    );
+
+    final editableTextFinder = find.byType(EditableTextWidget);
+    expect(editableTextFinder, findsOneWidget);
+
+    final editableTextWidget = tester.widget<EditableTextWidget>(
+      editableTextFinder,
+    );
+    expect(editableTextWidget.borderColor, Colors.black);
+  });
+
+  testWidgets('EditableTextWidget has a default border width', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Scaffold(body: EditableTextWidget(text: 'Hello')),
+      ),
+    );
+
+    final editableTextFinder = find.byType(EditableTextWidget);
+    expect(editableTextFinder, findsOneWidget);
+
+    final editableTextWidget = tester.widget<EditableTextWidget>(
+      editableTextFinder,
+    );
+    expect(editableTextWidget.borderWidth, 1.0);
+  });
+
+  testWidgets('EditableTextWidget has a default border radius', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Scaffold(body: EditableTextWidget(text: 'Hello')),
+      ),
+    );
+
+    final editableTextFinder = find.byType(EditableTextWidget);
+    expect(editableTextFinder, findsOneWidget);
+
+    final editableTextWidget = tester.widget<EditableTextWidget>(
+      editableTextFinder,
+    );
+    expect(editableTextWidget.borderRadius, 5.0);
+  });
+  testWidgets('EditableTextWidget has a default padding', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Scaffold(body: EditableTextWidget(text: 'Hello')),
+      ),
+    );
+
+    final editableTextFinder = find.byType(EditableTextWidget);
+    expect(editableTextFinder, findsOneWidget);
+
+    final editableTextWidget = tester.widget<EditableTextWidget>(
+      editableTextFinder,
+    );
+    expect(editableTextWidget.padding, 0.0);
+  });
+  testWidgets('EditableTextWidget has a default margin', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Scaffold(body: EditableTextWidget(text: 'Hello')),
+      ),
+    );
+
+    final editableTextFinder = find.byType(EditableTextWidget);
+    expect(editableTextFinder, findsOneWidget);
+
+    final editableTextWidget = tester.widget<EditableTextWidget>(
+      editableTextFinder,
+    );
+    expect(editableTextWidget.margin, 0.0);
+  });
+  testWidgets('EditableTextWidget has a default text width', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Scaffold(body: EditableTextWidget(text: 'Hello')),
+      ),
+    );
+
+    final editableTextFinder = find.byType(EditableTextWidget);
+    expect(editableTextFinder, findsOneWidget);
+
+    final editableTextWidget = tester.widget<EditableTextWidget>(
+      editableTextFinder,
+    );
+    expect(editableTextWidget.textWidth, 0.1);
+  });
+  testWidgets('EditableTextWidget has a default text height', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Scaffold(body: EditableTextWidget(text: 'Hello')),
+      ),
+    );
+
+    final editableTextFinder = find.byType(EditableTextWidget);
+    expect(editableTextFinder, findsOneWidget);
+
+    final editableTextWidget = tester.widget<EditableTextWidget>(
+      editableTextFinder,
+    );
+    expect(editableTextWidget.textHeight, 0.03);
+  });
+  testWidgets('EditableTextWidget has a default text padding', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Scaffold(body: EditableTextWidget(text: 'Hello')),
+      ),
+    );
+
+    final editableTextFinder = find.byType(EditableTextWidget);
+    expect(editableTextFinder, findsOneWidget);
+
+    final editableTextWidget = tester.widget<EditableTextWidget>(
+      editableTextFinder,
+    );
+    expect(editableTextWidget.textPadding, 0.0);
+  });
+  testWidgets('EditableTextWidget has a default text margin', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Scaffold(body: EditableTextWidget(text: 'Hello')),
+      ),
+    );
+
+    final editableTextFinder = find.byType(EditableTextWidget);
+    expect(editableTextFinder, findsOneWidget);
+
+    final editableTextWidget = tester.widget<EditableTextWidget>(
+      editableTextFinder,
+    );
+    expect(editableTextWidget.textMargin, 0.0);
+  });
+  testWidgets('EditableTextWidget has a default text', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Scaffold(body: EditableTextWidget(text: 'Hello')),
+      ),
+    );
+
+    final editableTextFinder = find.byType(EditableTextWidget);
+    expect(editableTextFinder, findsOneWidget);
+
+    final editableTextWidget = tester.widget<EditableTextWidget>(
+      editableTextFinder,
+    );
+    expect(editableTextWidget.text, 'Hello');
+  });
+  testWidgets('EditableTextWidget has a default text color', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Scaffold(body: EditableTextWidget(text: 'Hello')),
+      ),
+    );
+
+    final editableTextFinder = find.byType(EditableTextWidget);
+    expect(editableTextFinder, findsOneWidget);
+
+    final editableTextWidget = tester.widget<EditableTextWidget>(
+      editableTextFinder,
+    );
+    expect(editableTextWidget.textColor, Colors.black);
   });
 }
